087e189839e2e331de4e1bed0dd525bce060bfbd
Bryce Morgan <bamorgan13@gmail.com>
2019-03-18 14:15:08 -0400

    Create SuperComputerPlayer class

--- TicTacToe/lib/super_computer_player.rb
+++ TicTacToe/lib/super_computer_player.rb
@@ -2,6 +2,23 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+
+    ttt_node = TicTacToeNode.new(game.board, mark)
+
+    next_node = (ttt_node.children.select do |node|
+      node.winning_node?(mark)
+    end).first
+
+    if !next_node
+      next_node = (ttt_node.children.select do |node|
+      !node.losing_node?(mark)
+      end).first
+    end
+
+
+    next_node.prev_move_pos
+
+
   end
 end
 
